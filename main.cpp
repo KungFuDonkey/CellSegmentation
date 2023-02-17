2e2d4d1cd28bb4013a5be2f8fea520c266da7bb5
Sietze Riemersma <43845930+KungFuDonkey@users.noreply.github.com>
2023-02-17 16:57:01 +0100

    initial commit and setup of opencv

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+    // Read the image file
+    cv::Mat image = cv::imread("D:/My OpenCV Website/Eagle.jpg");
+    return 0;
+}
